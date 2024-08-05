d5054e1d69c84fa5ec0030805bb35a49711b4db6
leia uwu <leia@tutamail.com>
2024-08-05 12:14:21 -0300

    fix: production builds

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -18,9 +18,6 @@ export default defineConfig({
                     if (id.includes("node_modules")) {
                         return "vendor";
                     }
-                    if (id.includes("common")) {
-                        return "common";
-                    }
                 }
             }
         }
