6ac33c613dd44a2fdc454725322da8f142d95c5f
leia uwu <leia@tutamail.com>
2025-11-10 11:48:12 -0300

    refactor: remove useless comments from vector.ts
    now i barely have to scroll that file :)

--- common/src/utils/vector.ts
+++ common/src/utils/vector.ts
@@ -10,111 +10,49 @@ export interface Vector {
  * Vector util functions
  */
 export const Vec2 = {
-    /**
-     * Creates a new Vector
-     * @param x - The horizontal (x-axis) coordinate
-     * @param y - The vertical (y-axis) coordinate
-     * @returns A new Vector object with the provided x and y coordinates
-     */
     new(x: number, y: number): Vector {
         return { x, y };
     },
 
-    /**
-     * Sets Vector a to Vector b
-     * @param a - The first Vector
-     * @param b - The second Vector
-     */
     set(a: Vector, b: Vector) {
         a.x = b.x;
         a.y = b.y;
     },
 
-    /**
-     * Adds two Vectors together
-     * @param a - The first Vector
-     * @param b - The second Vector
-     * @returns A new Vector resulting from the addition of vectors a and b
-     */
     add(a: Vector, b: Vector): Vector {
         return Vec2.new(a.x + b.x, a.y + b.y);
     },
 
-    /**
-     * Adds two vectors together
-     * @param a - The first Vector
-     * @param x - The x-coordinate of the second vector
-     * @param y - The y-coordinate of the second vector
-     * @returns A new Vector resulting from the addition of a, and x and y
-     */
     add2(a: Vector, x: number, y: number): Vector {
         return Vec2.new(a.x + x, a.y + y);
     },
 
-    /**
-     * Subtracts one Vector from another
-     * @param a - The Vector to be subtracted from
-     * @param b - The Vector to subtract
-     * @returns A new Vector resulting from the subtraction of vector b from vector a
-     */
     sub(a: Vector, b: Vector): Vector {
         return Vec2.new(a.x - b.x, a.y - b.y);
     },
 
-    /**
-     * Subtracts one Vector from another
-     * @param a - The Vector to be subtracted from
-     * @param x - The x-coordinate of the second vector
-     * @param y - The y-coordinate of the second vector
-     * @returns A new Vector resulting from the subtraction of and x and y from vector a
-     */
     sub2(a: Vector, x: number, y: number): Vector {
         return Vec2.new(a.x - x, a.y - y);
     },
 
-    /**
-     * Multiplies a Vector by a scalar
-     * @param a - The Vector to be multiplied
-     * @param n - The scalar value to multiply the Vector by
-     * @returns A new Vector resulting from the multiplication of vector a and scalar n
-     */
     mul(a: Vector, n: number): Vector {
         return Vec2.new(a.x * n, a.y * n);
     },
 
-    /**
-     * Divides a Vector by a scalar
-     * @param a - The Vector to be divided
-     * @param n - The scalar value to divide the Vector by
-     * @returns A new Vector resulting from the division of vector a and scalar n
-     */
     div(a: Vector, n: number): Vector {
         return Vec2.new(a.x / n, a.y / n);
     },
 
-    /**
-     * Clones a Vector
-     * @param vector - The Vector to be cloned
-     * @returns A new Vector with the same coordinates as the input Vector
-     */
     clone(vector: Vector): Vector {
         return Vec2.new(vector.x, vector.y);
     },
 
-    /**
-     * Inverts a Vector
-     * @param a - The Vector to be inverted
-     * @returns A new Vector resulting from inverting vector a
-     */
     invert(a: Vector): Vector {
         return Vec2.new(-a.x, -a.y);
     },
 
     /**
-     * Rotates a Vector by a given angle
-     * @param vector - The Vector to be rotated
-     * @param angle - The angle in radians to rotate the Vector by
-     * @returns A new Vector resulting from the rotation of the input Vector by the given angle
+     * @param angle The angle in radians
      */
     rotate(vector: Vector, angle: number): Vector {
         const cos = Math.cos(angle);
@@ -122,50 +60,24 @@ export const Vec2 = {
         return Vec2.new(vector.x * cos - vector.y * sin, vector.x * sin + vector.y * cos);
     },
 
-    /**
-     * Calculates the squared length of a Vector
-     * @param a: The Vector
-     * @returns The squared length of Vector a
-     */
     lengthSqr(a: Vector): number {
         return a.x * a.x + a.y * a.y;
     },
 
-    /**
-     * Calculates the length of a Vector
-     * @param a: The Vector
-     * @returns The length of Vector a
-     */
     length(a: Vector): number {
         return Math.sqrt(Vec2.lengthSqr(a));
     },
 
-    /**
-     * Gets the distance between two vectors
-     * @param a - The first Vector
-     * @param b - The second Vector
-     * @returns The distance between Vector a and b
-     */
-    distance(a: Vector, b: Vector): number {
-        const diff = Vec2.sub(a, b);
-        return Vec2.length(diff);
-    },
-    /**
-     * Gets the distance between two vectors squared
-     * @param a - The first Vector
-     * @param b - The second Vector
-     * @returns The distance between Vector a and b squared
-     */
     distanceSqrt(a: Vector, b: Vector): number {
         const diff = Vec2.sub(a, b);
         return Vec2.lengthSqr(diff);
     },
 
-    /**
-     * Normalizes a Vector
-     * @param a: The Vector to be normalized
-     * @returns A new Vector resulting from normalizing the input Vector
-     */
+    distance(a: Vector, b: Vector): number {
+        const diff = Vec2.sub(a, b);
+        return Vec2.length(diff);
+    },
+
     normalize(a: Vector): Vector {
         const eps = 0.000001;
         const len = Vec2.length(a);
@@ -185,44 +97,29 @@ export const Vec2 = {
         };
     },
 
-    /**
-     * Interpolate between two Vectors
-     * @param start The start Vector
-     * @param end The end Vector
-     * @param interpFactor The interpolation factor ranging from 0 to 1
-     */
     lerp(start: Vector, end: Vector, interpFactor: number): Vector {
         return Vec2.add(Vec2.mul(start, 1 - interpFactor), Vec2.mul(end, interpFactor));
     },
 
-    /**
-     * Performs a dot product between two vectors
-     * @param a The first Vector
-     * @param b The second vector
-     * @returns The result of performing the dot product between the two Vectors
-     */
     dot(a: Vector, b: Vector): number {
         return a.x * b.x + a.y * b.y;
     },
+
     neg(vec: Vector): Vector {
         return { x: -vec.x, y: -vec.y };
     },
+
     perp(a: Vector): Vector {
         return { x: -a.y, y: a.x };
     },
-    /**
-     * Tests whether two `Vectors` are equal, within a certain tolerance
-     * @param a The first `Vector`
-     * @param b The second `Vector`
-     * @param epsilon The largest difference in any component that will be accepted as being "equal"
-     * @returns Whether or not the two vectors are considered equal with the given epsilon
-     */
+
     equals(a: Vector, b: Vector, epsilon = 0.001): boolean {
         return Math.abs(a.x - b.x) <= epsilon && Math.abs(a.y - b.y) <= epsilon;
     },
+
     /**
      * Takes a polar representation of a vector and converts it into a cartesian one
-     * @param angle The vector's angle
+     * @param angle The angle in radians
      * @param magnitude The vector's length. Defaults to 1
      * @returns A new vector whose length is `magnitude` and whose direction is `angle`
      */
