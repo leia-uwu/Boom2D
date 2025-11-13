61bdc7459472e238263f01e71d2bca5dd14d50a7
leia uwu <leia@tutamail.com>
2025-11-13 01:13:08 -0300

    refactor: refactor logger

--- server/src/logger.ts
+++ server/src/logger.ts
@@ -1,67 +1,73 @@
-export const Colors = {
-    black: {
-        normal: 30,
-        bright: 90,
-    },
-    red: {
-        normal: 31,
-        bright: 91,
-    },
-    green: {
-        normal: 32,
-        bright: 92,
-    },
-    yellow: {
-        normal: 33,
-        bright: 93,
-    },
-    blue: {
-        normal: 34,
-        bright: 94,
-    },
-    magenta: {
-        normal: 35,
-        bright: 95,
-    },
-    cyan: {
-        normal: 36,
-        bright: 96,
-    },
-    white: {
-        normal: 37,
-        bright: 97,
-    },
-    default: {
-        normal: 39,
-        bright: 39,
-    },
-} as const;
+import { styleText } from "node:util";
 
-type Color = keyof typeof Colors;
-type Variant = keyof (typeof Colors)[Color];
-
-const CSI = "\u001B";
-export function styleText(
-    string: string,
-    ...styles: Array<(typeof Colors)[Color][Variant]>
-): string {
-    return `${CSI}[${styles.join(";")}m${string}${CSI}[0m`;
+export interface LoggerConfig {
+    logDate: boolean;
+    debug: boolean;
+    warn: boolean;
+    error: boolean;
 }
 
-export class Logger {
-    constructor(public prefix: string) {}
+const defaultLoggerConfig: LoggerConfig = {
+    logDate: true,
+    debug: true,
+    warn: true,
+    error: true,
+};
 
-    log(...message: any[]): void {
-        const date = new Date();
-        const dateString = `[${date.toISOString().substring(0, 10)} ${date.toLocaleTimeString()}]`;
-        console.log(
-            styleText(dateString, Colors.cyan.normal),
-            styleText(this.prefix, Colors.green.normal),
-            "|",
-            message.join(" "),
-        );
+export class Logger {
+    config: LoggerConfig;
+    prefix: string;
+
+    constructor(
+        prefix: string,
+        config: Partial<LoggerConfig> = {},
+    ) {
+        this.config = {
+            ...defaultLoggerConfig,
+            ...config,
+        };
+
+        this.prefix = prefix;
     }
+
+    private _log(fn: typeof console.log, ...message: any[]): void {
+        if (this.config.logDate) {
+            const date = new Date();
+            const dateString = `[${
+                date.toISOString().substring(0, 10)
+            } ${date.toLocaleTimeString()}]`;
+
+            fn(
+                styleText("cyan", dateString),
+                styleText("green", this.prefix),
+                "|",
+                ...message,
+            );
+        } else {
+            fn(
+                styleText("green", this.prefix),
+                "|",
+                ...message,
+            );
+        }
+    }
+
+    log(...message: any[]) {
+        this._log(console.log, ...message);
+    }
+
+    debug(...message: any[]) {
+        if (!this.config.debug) return;
+        this._log(console.debug, ...message);
+    }
+
     warn(...message: any[]): void {
-        this.log(styleText("[WARNING]", Colors.yellow.normal), message.join(" "));
+        if (!this.config.warn) return;
+        this._log(console.warn, styleText("yellow", "[WARNING]"), ...message);
+    }
+
+    error(...message: any[]): void {
+        if (!this.config.error) return;
+        this._log(console.error, styleText("yellow", "[WARNING]"), ...message);
     }
 }
